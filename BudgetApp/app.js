95dc4743957db0629b4ce897f5b3a4166dd25bfb
Jerry V <jvr572@gmail.com>
2020-02-01 19:11:37 -0500

    adding item to budget

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -59,7 +59,9 @@ var UIController =(function(){
     inputType: '.add__type', 
     inputDescription: '.add__description', 
     inputValue: '.add__value', 
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   };
 
   return{
@@ -69,6 +71,27 @@ var UIController =(function(){
         description: document.querySelector(DOMStrings.inputDescription).value,
         value: document.querySelector(DOMStrings.inputValue).value
       };
+    },
+
+    addListItem: (obj, type) => {
+      let html, newHtml, element;
+      
+      //create HTML placeholder
+      if(type === 'inc'){
+        element = DOMStrings.incomeContainer;
+
+        html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+      } else if (type === 'exp'){ 
+        element = DOMStrings.expensesContainer;
+        
+        html = ' <div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%<div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+      }
+      //replace placeholder text 
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+      // Insert HTML into DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
     getDOMStrings: () => DOMStrings 
@@ -101,7 +124,7 @@ var controller = ((budgetCtrl, UICtrl) => {
     // add the item
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // add to UI 
-
+    UICtrl.addListItem(newItem, input.type);
     // calc the budget 
 
     // display the budget on UI 
