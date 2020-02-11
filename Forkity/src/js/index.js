5e1d576b34ca1769ea2689363f730cd849f49932
Jerry V <jvr572@gmail.com>
2020-02-11 12:30:27 -0500

    implenting UI

--- Forkity/src/js/index.js
+++ Forkity/src/js/index.js
@@ -1,13 +1,38 @@
-import axios from 'axios';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
+/** Global state of the app
+ * - Search object 
+ * - Current State
+ * - Shopping List 
+ * - Liked recipes
+  */
 
-async function getResults(query)  {
-  try {
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
+const state = {};
+
+const controlSearch = async () => {
+  // 1. get query from view 
+  const query = searchView.getInput();
+
+  if(query){
+    // 2 search object and add to state 
+    state.search = new Search(query);
+
+    // 3 Prepare Ui for results 
+    searchView.clearInput();
+    searchView.clearResults();
+    //  4 Search for recipes
+    await state.search.getResults();
+
+    // 5 Render results on UI
+    searchView.renderResults(state.search.result);
   }
 };
 
-getResults('pizza')
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
+
+
--- Forkity/src/js/models/Search.js
+++ Forkity/src/js/models/Search.js
@@ -1,4 +1,21 @@
-// Replace 
-// `https://forkify-api.herokuapp.com/api/search?&q=${this.query}`)
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults()  {
+    try {
+      const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+      this.result = res.data.recipes;
+      // console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+}
+
 // replace in Recipe.js
 // `https://forkify-api.herokuapp.com/api/get?rId=${this.id}`;
