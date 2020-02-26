c6135ddbe68a0bab5606ffc344c0b75407db48da
Jerry V <jvr572@gmail.com>
2020-02-26 18:11:15 -0500

    need to fix minor UI issues

--- Forkity/src/js/index.js
+++ Forkity/src/js/index.js
@@ -1,10 +1,15 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
+import List from "./models/List";
+import Likes from "./models/Likes";
+
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
 import * as listView from "./views/listView";
+import * as likesView from "./views/likesView";
+
 import { elements, renderLoader, clearLoader } from "./views/base";
-import List from "./models/List";
+
 /** Global state of the app
  * - Search object
  * - Current State
@@ -26,6 +31,7 @@ const controlSearch = async () => {
 		searchView.clearInput();
 		searchView.clearResults();
 		renderLoader(elements.searchRes);
+
 		try {
 			//  4 Search for recipes
 			await state.search.getResults();
@@ -81,7 +87,7 @@ const controlRecipe = async () => {
 
 			// render recipe
 			clearLoader();
-			recipeView.renderRecipe(state.recipe);
+			recipeView.renderRecipe(state.recipe, state.likes.isLiked(id));
 			// console.log(state.recipe);
 		} catch (error) {
 			alert("Error processing recipe!");
@@ -108,8 +114,7 @@ const controlList = () => {
 
 // Handle delete and update list items event
 elements.shopping.addEventListener("click", e => {
-	const id = e.target.closest('.shopping__item').dataset.itemid
-	console.log(e.target.closest('.shopping__item'), id);
+	const id = e.target.closest(".shopping__item").dataset.itemid;
 	// Handle the delete button
 	if (e.target.matches("shopping__delete, .shopping__delete *")) {
 		// Delete from state
@@ -121,6 +126,45 @@ elements.shopping.addEventListener("click", e => {
 		const val = parseFloat(e.target.value);
 		state.list.updateCount(id, val);
 	}
+});
+
+/////LIKE CONTROLLER
+
+const controlLike = () => {
+	console.log(state);
+	if (!state.likes) state.likes = new Likes();
+
+	const curId = state.recipe.id;
+
+	// User has not liked the current recipe
+	if (!state.likes.isLiked(curId)) {
+		const newLike = state.likes.addLike(
+			curId,
+			state.recipe.title,
+			state.recipe.author,
+			state.recipe.img,
+		);
+		likesView.toggleLikeBtn(true);
+
+		likesView.renderLike(newLike);
+	} else {
+		// remove the like from state
+		state.likes.deleteLike(curId);
+		likesView.toggleLikeBtn(false);
+		likesView.deleteLike(curId);
+	}
+	likesView.toggleLikeMenu(state.likes.getNumLikes());
+};
+
+window.addEventListener("load", () => {
+  
+  state.likes = new Likes();
+
+	state.likes.readData();
+
+	likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+	state.likes.likes.forEach(like => likesView.renderLike(like));
 });
 
 // handling recipe button clicks
@@ -136,6 +180,10 @@ elements.recipe.addEventListener("click", e => {
 		state.recipe.updateServings("inc");
 		recipeView.updateServingsIngredients(state.recipe);
 	} else if (e.target.matches("recipe__btn--add, .recipe__btn--add *")) {
+		// Add ingredient to shopping list
 		controlList();
+	} else if (e.target.matches(".recipe__love, .recipe__love *")) {
+		// calls the like controller
+		controlLike();
 	}
 });
--- Forkity/src/js/models/Likes.js
+++ Forkity/src/js/models/Likes.js
@@ -1,24 +1,38 @@
 export default class Likes {
-  constructor() {
-    this.likes = [];
-  }
+	constructor() {
+		this.likes = [];
+	}
 
-  addLike(id, title, author, img){
-    const like = { id, title, author, img };
-    this.likes.push(like);
-    return like;
-  }
+	addLike(id, title, author, img) {
+		const like = { id, title, author, img };
+		this.likes.push(like);
 
-  deleteLike(id){
-    const idx = this.likes.findIndex(el => el.id === id);
-    this.likes.splice(idx, 1);
-  }
+		this.persistData();
 
-  isliked(id){
-    return this.likes.findIndex(el => el.id === id)
-  }
+		return like;
+	}
 
-  getLikes(){
-    return this.likes.length;
-  }
+	deleteLike(id) {
+		const idx = this.likes.findIndex(el => el.id === id);
+		this.likes.splice(idx, 1);
+
+		this.persistData();
+	}
+
+	isLiked(id) {
+		return this.likes.findIndex(el => el.id === id) !== -1;
+	}
+
+	getNumLikes() {
+		return this.likes.length;
+	}
+
+	persistData() {
+		localStorage.setItem("likes", JSON.stringify(this.likes));
+	}
+
+	readData() {
+		const storage = JSON.parse(localStorage.getItem("likes"));
+		if (storage) this.likes = storage;
+	}
 }
