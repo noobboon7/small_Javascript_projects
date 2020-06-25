f215032208802672aff34f63452a60b84a36a422
Jerry V <jvr572@gmail.com>
2020-06-24 22:36:29 -0400

    able to move ship css needs work

--- SpaceInvaders/app.js
+++ SpaceInvaders/app.js
@@ -14,7 +14,28 @@ document.addEventListener('DOMContentLoaded', () => {
     15,16,17,18,19,20,21,22,23,24,
     30,31,32,33,34,35,36,37,38,39
   ];
+  // add invaders to the board
+  alienInvaders.forEach(invader => squares[invader].classList.add('invader'));
+  // add shooter to the board
+  squares[curShooterIdx].classList.add('shooter');
 
-  alienInvaders.forEach(invader => squares[curInvaderIdx + invader].classList.add('invader'))
+  // controls 
+  function moveShooter(e) {
+    squares[curShooterIdx].classList.remove('shooter');
+    
+    switch (e.keyCode) {
+      case 37:
+        if (curShooterIdx % width !== 0) curShooterIdx--;
+        break;
+      case 39:
+        if (curShooterIdx % width < width - 1) curShooterIdx++;
+        break;
+    }
+    squares[curShooterIdx].classList.add('shooter');  
+  }
+
+  document.addEventListener('keydown', moveShooter);
+
+  
 
 });
