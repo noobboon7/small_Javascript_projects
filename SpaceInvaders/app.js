25a3bf3315c139aff01b574eebe26f8ed989bdeb
Jerry V <jvr572@gmail.com>
2020-06-24 23:14:18 -0400

    moving invaders around

--- SpaceInvaders/app.js
+++ SpaceInvaders/app.js
@@ -36,6 +36,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.addEventListener('keydown', moveShooter);
 
-  
+  function moveInvaders() {
+    const leftEdge = alienInvaders[0] % width === 0;
+    const rightEdge = alienInvaders[alienInvaders.length -1] % width === width - 1;
 
+    if ((leftEdge && direction === -1) || (rightEdge && direction === 1)) {
+      direction = width;
+    } else if (direction === width) {
+      if (leftEdge) direction = 1;
+      else direction = -1;
+    } 
+    for (let i = 0; i <= alienInvaders.length-1; i++) {
+      squares[alienInvaders[i]].classList.remove('invader');
+    }
+    for (let i = 0; i <= alienInvaders.length-1; i++) {
+      alienInvaders[i] += direction;
+    }
+    for (let i = 0; i <= alienInvaders.length-1; i++) {
+      squares[alienInvaders[i]].classList.add('invader');
+    }
+    
+    // check for overlapping; game over
+    if(squares[curShooterIdx].classList.contains('invader', 'shooter')){
+      resultDisplay.textContent = 'Game Over';
+      squares[curShooterIdx].classList.add('boom');
+      clearInterval(invaderId);
+    }
+    
+    for (let i = 0; i <= alienInvaders.length-1; i++) {
+      if(alienInvaders[i] > (squares.length - (width-1))){
+        resultDisplay.textContent = "Game Over";
+        clearInterval(invaderId);
+      }
+    }
+    
+  }
+  invaderId = setInterval(moveInvaders, 500);
 });
