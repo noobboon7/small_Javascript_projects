63f368b6a0da8b48d7f767b26285f0559bb4d290
Jerry V <jvr572@gmail.com>
2020-07-11 22:48:09 -0400

    playing around with charts

---
+++ ChartingJs/index.js
@@ -0,0 +1,12 @@
+JSC.Chart("chartDiv", {
+	type: "horizontal column",
+	series: [
+		{
+			points: [
+				{ x: "Apples", y: 50 },
+				{ x: "Oranges", y: 42 },
+				{ x: "crans", y: 12 },
+			],
+		},
+	],
+});
